8b54c38b8813dd3225e418f963b198307dc2e95b
aksanachyrmasheva <aksanachyrmasheva@gmail.com>
2023-04-18 18:40:37 +0300

    Initial commit

---
+++ src/Class.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class Class {
+    private int number ;
+    private String word;
+    private int[] array;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+    }
+
+    @Override
+    public String toString() {
+        return "Class{" +
+                "number=" + number +
+                ", word='" + word + '\'' +
+                ", array=" + Arrays.toString(array) +
+                '}';
+    }
+
+    public Class(int number, String word, int[] array) {
+        this.number = number;
+        this.word = word;
+        this.array = array;
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+
+        Class clases = new Class(99,"Java",new int[]{45,78});
+        System.out.println(clases);
+
+
+
+
+    }
+}
